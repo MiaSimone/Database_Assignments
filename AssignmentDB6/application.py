5660fcd255145c16f34e051f10c972e1face7a36
MiaSimone <cph-md291@cphbusiness.dk>
2022-05-14 16:19:28 +0200

    might be done?

--- AssignmentDB6/application.py
+++ AssignmentDB6/application.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify, render_template
 import redis
 import json
 from json2html import *
+import time
 
 app = Flask(__name__, template_folder='hmtl_documents')
 
@@ -18,27 +19,67 @@ with r.pipeline() as pipe:
         pipe.execute()
 r.bgsave()
     
-def getData(n1, n2) -> dict:
+def getData(n1, n2):
     dataList = []
-    for n in range(n1, n2+1):
-        dataList.append(r.hgetall(str(n)))
+    tic = time.perf_counter()
+    for n in range(int(n1), int(n2)+1):
+        #print(n)
+        #print(r.hgetall(str(n)))
+        d = r.hgetall(str(n))
+        dataList.append(d)
+    toc = time.perf_counter()
+    dataList.append(toc - tic)
+    return dataList
+
+def getAttributes():
+    tic = time.perf_counter()
+    unPrettyList = list(r.hgetall('1'))
+    toc = time.perf_counter()
+    prettyList = []
+    for s in unPrettyList:
+        txt = str(s).split("'")
+        prettyList.append(txt[1])
+    time2 = toc - tic
+    prettyList.append(time2)
+    return prettyList
+
+def getBySpecificAtt(n, s):
+    tic = time.perf_counter()
+    string = r.hget(n,s)
+    toc = time.perf_counter()
+    time2 = toc - tic
+    txt = str(string).split("'")
+    
+    dataList = []
+    dataList.append(txt[1])
+    dataList.append(time2)
     return dataList
 
 
 # ---------------- THE ENDPOINTS --------------------
 @app.route('/')
 def start():
-    return render_template('start.html')
+    attributes = getAttributes()
+    
+    return render_template('start.html', attributes=attributes[1:-1], time=attributes[-1])
 
 @app.route('/data', methods=['GET', 'POST'])
 def data():
     if request.method == 'POST':
         n1 = request.form['n1']
         n2 = request.form['n2']
-        data = getData(int(n1), int(n2))
+        data = getData(n1, n2)
           
-        return render_template("data.html", content=data)
+        return render_template("data.html", data=data[int(n1):int(n2)], time=data[-1])
     
+@app.route('/specificData', methods=['GET', 'POST'])
+def specificData():
+    if request.method == 'POST':
+        n = request.form['n']
+        s = request.form['s']
+        data = getBySpecificAtt(n, s)
+          
+        return render_template("specificData.html", data=data[0], time=data[1])
 
 @app.route('/redis', methods=['GET', 'POST'])
 def predict():
